8fc2990951c7c62838301094989861a6dbe41ba3
Tuong <ttruong2621@sdsu.edu>
2026-03-06 11:21:09 -0800

    updating the settings.py file with variable of .env file

--- src/app/core/settings.py
+++ src/app/core/settings.py
@@ -1,9 +1,30 @@
 from pydantic_settings import BaseSettings
+from pydantic import Field
 
 
 class Settings(BaseSettings):
     app_name: str = "Learning Path Generator"
     app_version: str = "1.0.0"
+
+    secret_key = Field(
+        default="dev_secret",
+        description="The secret key for JWT",
+    )
+
+    algorithm: str = Field(
+        default="Algorithm",
+        description="The algorithm used for JWT",
+    )
+
+    access_token_expire_minutes: int = Field(
+        default=10,
+        description="Access token expiration time in minutes",
+    )
+
+
+class Config:
+    env_file = ".env"
+    env_file_encoding = "utf-8"
 
 
 settings = Settings()
