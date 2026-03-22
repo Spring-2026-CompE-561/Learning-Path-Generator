73ac3c826f7658d0953c9569b85f85ec33aafb70
Tuong <ttruong2621@sdsu.edu>
2026-03-21 18:02:13 -0700

    finish create user service to database using hashed password

--- src/app/services/user.py
+++ src/app/services/user.py
@@ -1,10 +1,31 @@
+from core.auth import get_password_hash
+from app.repository.user import UserRepository
+from sqlalchemy.orm import Session
+
+from app.schemas.user import UserDB, UserCreate, User
+
+
 class UserService:
     """Service class for user-related operations."""
 
-    def __init__(self, db):
-        self.db = db
+    def __init__(self) -> None:
+        """Initialize the UserService with a UserRepository instance."""
+        self.repository = UserRepository()
 
-    def register_user(self, user_data):
+    "Return a user model, database object"
+
+    def register_user(self, db: Session, user_db: UserCreate) -> User:
+        "FIXME: adding validation"
         """Register a new user.
-        This method will take user data, validate it, and create a new user in the database.
+        create a new user in the database.
         """
+        # Hash the password before creating the user
+        hashed_password = get_password_hash(user_db.password)
+        user_db = UserDB(
+            email=user_db.email,
+            hashed_password=hashed_password,
+        )
+        return self.repository.create_user(db, user_db)
+
+
+user_service = UserService()
