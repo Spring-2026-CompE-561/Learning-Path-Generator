36fbd3d3797c5fffb8efe1a8374ea810e726c02f
David Soriano <davidsoriano707@gmail.com>
2026-03-24 21:38:52 -0700

    added resource schema

---
+++ src/app/schemas/resource.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel, Field
+from typing import Literal
+
+# no resource creation schema needa because ai will generate it 
+
+class ResourceBase(BaseModel):
+    """base schema that will be used by the other resource response schema"""
+
+    # literal restricts to a specific choice so it tells you out of the choices what this type is
+    resource_type: Literal["video", "audio", "article", "problems", "course"]
+
+    # Field sets max length to 300 and ... makes it req, used to describe what your resource does
+    resource_summary: str = Field(..., max_length = 300)
+
+    url: str = Field(..., max_length = 2000)
+
+
+class ResourceUpdate(BaseModel):
+    """meant to be used if they want to switch the resources given, required to give description as to what they want instead for better resources"""
+    message: str
+
+
+
+class Resource(ResourceBase):
+    """response that is given to frontend when resource is viewed or updated """
+    id: int
+
+    # makes it so that pydantic can understand sqlalchemy
+    model_config = {"from_attributes": True}
