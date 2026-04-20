391efbbbe3aa764e2c8d30df0ef7754f3e4d8723
tuong <ttruong2621@sdsu.edu>
2026-04-19 17:53:11 -0700

    Finish Initialize header #71

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Image from "next/image";
+
+export default function Dashboard() {
+  return (
+
+    <div className="flex w-fill flex-col items-center justify-center gap-8 py-20"> Hello World! </div>
+    
+  );
+}
