9946b22e785c97b0f58caba04a46f11b0f3c556e
tuong <ttruong2621@sdsu.edu>
2026-04-20 14:46:58 -0700

    Updating header: w-full, reponsive design, grey background, and initialize navbar

---
+++ frontend/src/components/ui/navbar.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { usePathname } from 'next/navigation';
+import {Button, buttonVariants } from "@/components/ui/button";
+
+export function Navbar() {
+    <div className="w-full h-fit flex items-center justify-between p-10">
+
+    </div>
+}
