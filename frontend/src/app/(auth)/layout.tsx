c01a7ebdea4ffc3891576e3630a0c38e04ebaff3
tuong <ttruong2621@sdsu.edu>
2026-04-28 17:01:11 -0700

    Finishing validate token in every single pages. #84

--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -8,23 +8,58 @@ import { useRouter } from "next/navigation";
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
 
     //State to check authentication status
-    const [checkingAuth, setCheckingAuth] = useState(true);
+    const [loading, setLoading] = useState(true);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
     //router for redirection
     const router = useRouter();
 
     //check authentication immediately when component mounts
+    //if failed, redirect to landing page
     useEffect(() => {
-        const token = localStorage.getItem("access_token");
-        if(!token){
-            router.push("/")
-            return;
+        async function checkAuth(){
+            const token = localStorage.getItem("access_token");
+            
+            if(!token){
+                router.replace("/")
+                return;
+            }
+
+            try {
+                const res = await fetch("http://127.0.0.1:8000/users/me", {
+                    method: "GET",
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        Accept: "application/json"
+                    },
+                });
+
+                if(!res.ok){
+                    localStorage.removeItem("access_toekn");
+                    router.replace("/");
+                    return;
+                }
+
+                setIsAuthenticated(true);
+            } catch (error){
+                localStorage.removeItem("access_token");
+                router.replace("/");
+            } finally {
+                setLoading(false);
+            }
+
         }
-        setCheckingAuth(false);
+        
+        checkAuth();
+        setLoading(false);
     }, [router]); 
     
     //Consider removing this in real implementation
-    if(checkingAuth) {
+    if(loading) {
         return <div>Checking authentication...</div>;
+    }
+
+    if(!isAuthenticated){
+        return null;
     }
 
     return <>{children}</>
