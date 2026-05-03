924afb834116cde9599c4c8ca42ec922e53dc327
tuong <ttruong2621@sdsu.edu>
2026-05-03 16:06:07 -0700

    Initialize user account form

--- frontend/src/app/(auth)/account/page.tsx
+++ frontend/src/app/(auth)/account/page.tsx
@@ -5,11 +5,7 @@ export default function Account() {
   return (
 
     <div className="flex w-full flex-col items-center justify-center gap-8 py-20">
-      
-
-      <h2 className="text-2xl">Account Seetings</h2>
-      <p>Manage your profile and preference</p>
-      <AccountForm />
+      <AccountForm className="w-[50%]" />
     </div>
     
   );
