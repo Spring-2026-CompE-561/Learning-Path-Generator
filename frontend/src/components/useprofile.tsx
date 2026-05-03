3c23d148669e95349300cf3c78fa9ad616358aa9
tuong <ttruong2621@sdsu.edu>
2026-05-03 16:13:48 -0700

    Adding header to the account formq

--- frontend/src/components/useprofile.tsx
+++ frontend/src/components/useprofile.tsx
@@ -9,18 +9,18 @@ import * as z from "zod"
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
 } from "@/components/ui/card"
 import {
-  Field,
-  FieldDescription,
-  FieldGroup,
-  FieldLabel,
-  FieldError,
+    Field,
+    FieldDescription,
+    FieldGroup,
+    FieldLabel,
+    FieldError,
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
 import { routerServerGlobal } from "next/dist/server/lib/router-utils/router-server-context"
@@ -29,179 +29,190 @@ import { routerServerGlobal } from "next/dist/server/lib/router-utils/router-ser
 
 //form schema for validation using zod
 const formSchema = z.object({
-  email: z.string()
-          .trim()
-          .min(1, "Email is required")
-          .email("Invalid email address"),
-  username: z.string()
-            .trim()
-            .min(3, "Username must be at least 3 characters")
-            .max(20, "Username must be at most 20 characters"),
-  password: z.string()
-            .trim()
-            .min(8, "Password must be at least 8 characters")
-            .max(10, "Password must be at most 10 characters"),
-            confirmPassword: z.string()
-    })
+    email: z.string()
+        .trim()
+        .min(1, "Email is required")
+        .email("Invalid email address"),
+    username: z.string()
+        .trim()
+        .min(3, "Username must be at least 3 characters")
+        .max(20, "Username must be at most 20 characters"),
+    password: z.string()
+        .trim()
+        .min(8, "Password must be at least 8 characters")
+        .max(10, "Password must be at most 10 characters"),
+    confirmPassword: z.string()
+})
 //function to be called on successful registration
 type SigninFormProps = React.ComponentProps<"div"> & {
-  onRegisterSuccess?: () => void
+    onRegisterSuccess?: () => void
 }
 
-export function AccountForm({className, onRegisterSuccess, ...props
+export function AccountForm({ className, onRegisterSuccess, ...props
 }: SigninFormProps) {
-  //function to handle form submission
-  
-  //hook to call
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      email: "",
-      username: "",
-      password: "",
-      confirmPassword: "",
-    }
-  })
+    //function to handle form submission
 
-  async function onSubmit(data: z.infer<typeof formSchema>){
-    //sending http request
-    const res = await fetch("http://127.0.0.1:8000/users/register", {
-      method: "POST",
-      headers: {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
+    //hook to call
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            username: "",
+            password: "",
+            confirmPassword: "",
+        }
     })
 
-    //handling error response
-    if(!res.ok){
-      const errorData = await res.json()
-      toast.error(`Registration failed: ${errorData?.message || "Unknown error"}`, {
-        position: "top-center",
-      })
-      return
+    async function onSubmit(data: z.infer<typeof formSchema>) {
+        //sending http request
+        const res = await fetch("http://127.0.0.1:8000/users/register", {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        })
+
+        //handling error response
+        if (!res.ok) {
+            const errorData = await res.json()
+            toast.error(`Registration failed: ${errorData?.message || "Unknown error"}`, {
+                position: "top-center",
+            })
+            return
+        }
+
+        //waiting backend reponse
+        const returnedData = await res.json();
+
+        //FIXME: For testing using the uncomment code
+        //The comment code is for successful login, for real implementation
+        // toast.success("Login successful!",
+        //   {position: "top-center"})
+        //   form.reset()
+
+        //code to display submited input as JSON in the toast
+        toast("Here is your Account Information:", {
+            description: (
+                <pre className="mt-2 w-[320px] overflow-x-auto rounded-md bg-code p-4 text-code-foreground">
+                    <code>
+                        {res.status === 201 ?
+                            "Account created Successfully ! \n You can now log in with your credentials." :
+                            "Unexpected response from server. Please try again later."
+                        }
+                    </code>
+                </pre>
+            ),
+            position: "bottom-right",
+            classNames: {
+                content: "flex flex-col gap-2",
+            },
+            style: {
+                "--border-radius": "calc(var(--radius)  + 4px)",
+            } as React.CSSProperties,
+        });
+
+        //reset form after successful submission
+        //and confirm the resiter is successfull
+        if (res.status === 201) {
+            form.reset()
+            onRegisterSuccess?.()
+        }
+
+        return returnedData;
+
     }
-    
-    //waiting backend reponse
-    const returnedData = await res.json();
-    
-    //FIXME: For testing using the uncomment code
-    //The comment code is for successful login, for real implementation
-    // toast.success("Login successful!",
-    //   {position: "top-center"})
-    //   form.reset()
-    
-    //code to display submited input as JSON in the toast
-    toast("Here is your Account Information:", {      
-      description: (
-        <pre className="mt-2 w-[320px] overflow-x-auto rounded-md bg-code p-4 text-code-foreground">
-          <code>
-            {res.status === 201?
-             "Account created Successfully ! \n You can now log in with your credentials." :
-             "Unexpected response from server. Please try again later."
-            }
-            </code>
-        </pre>
-      ),
-      position: "bottom-right",
-      classNames: {
-        content: "flex flex-col gap-2",
-      },
-      style: {
-        "--border-radius": "calc(var(--radius)  + 4px)",
-      } as React.CSSProperties,
-    });
-    
-    //reset form after successful submission
-    //and confirm the resiter is successfull
-    if(res.status === 201){
-      form.reset()
-      onRegisterSuccess?.()
-    }
+    return (
+        <div className={cn("flex flex-col gap-6", className)} {...props}>
+            <Card>
+                <div className="flex flex-col justify-start">
+                    <CardHeader>
+                        <div>
+                            <h2 className="text-5xl">Account Settings</h2>
+                        </div>
+                        <CardDescription>
+                            <p>Manage your profile and preferences</p>
+                        </CardDescription>
+                    </CardHeader>
+                </div>
 
-    return returnedData;
+                <CardContent>
+                    <form id="signin-form" onSubmit={form.handleSubmit(onSubmit)}>
+                        <FieldGroup>
+                            {/*Email Field */}
+                            <Controller
+                                name="email"
+                                control={form.control}
+                                render={({ field, fieldState }) => (
+                                    <Field data-invalid={fieldState.invalid}>
+                                        <FieldLabel htmlFor="email">Email</FieldLabel>
+                                        <Input
+                                            {...field}
+                                            id="email"
+                                            aria-invalid={fieldState.invalid}
+                                            placeholder="m@example.com"
+                                            autoComplete="off"
+                                        />
+                                        {fieldState.invalid && (
+                                            <FieldError errors={[fieldState.error]} />
+                                        )}
+                                    </Field>
+                                )}
+                            />
+                            {/*User-Name Field */}
+                            <Controller
+                                name="username"
+                                control={form.control}
+                                render={({ field, fieldState }) => (
+                                    <Field data-invalid={fieldState.invalid}>
+                                        <FieldLabel htmlFor="username">Username</FieldLabel>
+                                        <Input
+                                            {...field}
+                                            id="username"
+                                            aria-invalid={fieldState.invalid}
+                                            placeholder="john_doe"
+                                            autoComplete="off"
+                                        />
+                                        {fieldState.invalid && (
+                                            <FieldError errors={[fieldState.error]} />
+                                        )}
+                                    </Field>
+                                )}
+                            />
 
-  }
-  return (
-    <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <Card>
-        <CardContent>
-          <form id="signin-form" onSubmit={form.handleSubmit(onSubmit)}>
-            <FieldGroup>
-              {/*Email Field */}
-              <Controller
-                name="email"
-                control={form.control}
-                render={({field, fieldState}) => (
-                  <Field data-invalid={fieldState.invalid}>
-                    <FieldLabel htmlFor="email">Email</FieldLabel>
-                    <Input
-                      {...field}
-                      id="email"
-                      aria-invalid={fieldState.invalid}
-                      placeholder="m@example.com"
-                      autoComplete="off"
-                    />
-                    {fieldState.invalid && (
-                      <FieldError errors={[fieldState.error]} />
-                    )}
-                  </Field>
-                )}
-              />
-             {/*User-Name Field */}
-              <Controller
-                name="username"
-                control={form.control}
-                render={({field, fieldState}) => (
-                  <Field data-invalid={fieldState.invalid}>
-                    <FieldLabel htmlFor="username">Username</FieldLabel>
-                    <Input
-                      {...field}
-                      id="username"
-                      aria-invalid={fieldState.invalid}
-                      placeholder="john_doe"
-                      autoComplete="off"
-                    />
-                    {fieldState.invalid && (
-                      <FieldError errors={[fieldState.error]} />
-                    )}
-                  </Field>
-                )}
-              />
+                            {/*Password Field */}
+                            <Controller
+                                name="password"
+                                control={form.control}
+                                render={({ field, fieldState }) => (
+                                    <Field data-invalid={fieldState.invalid}>
+                                        <div className="flex items-center">
+                                            <FieldLabel htmlFor="password">Password</FieldLabel>
+                                        </div>
+                                        <Input
+                                            {...field}
+                                            id="password"
+                                            aria-invalid={fieldState.invalid}
+                                            type="password"
+                                            placeholder="********"
+                                            autoComplete="off"
+                                        />
+                                        {fieldState.invalid && (
+                                            <FieldError errors={[fieldState.error]} />
+                                        )}
 
-              {/*Password Field */} 
-              <Controller 
-                name="password"
-                control={form.control}
-                render={({field, fieldState}) => (
-                  <Field data-invalid={fieldState.invalid}>
-                    <div className="flex items-center">
-                      <FieldLabel htmlFor="password">Password</FieldLabel>
-                    </div>
-                    <Input 
-                        {...field}
-                        id="password"
-                        aria-invalid={fieldState.invalid} 
-                        type="password" 
-                        placeholder="********"
-                        autoComplete="off"
-                    />
-                    {fieldState.invalid && (
-                      <FieldError errors={[fieldState.error]} />
-                    )}
+                                    </Field>
+                                )}
+                            />
+                            <Field>
+                                <Button type="submit">Save</Button>
+                            </Field>
 
-                  </Field>
-                  )}
-              />
-              <Field>
-                  <Button type="submit">Sign in</Button>
-              </Field>
-  
-            </FieldGroup>
-          </form>
-        </CardContent>
-      </Card>
-    </div>
-  )
+                        </FieldGroup>
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
+    )
 }
