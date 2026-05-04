0ddaaa58695f79cce2b78e57fa7f9fa7060eea3d
tuong <ttruong2621@sdsu.edu>
2026-05-04 12:19:26 -0700

    Initialize footer components

---
+++ frontend/src/components/ui/footer.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { usePathname } from 'next/navigation';
+import { DialogLogin } from "@/components/uselogDialog";
+import { DialogSignin } from "@/components/useSignDialog";
+import * as React from "react";
+
+
+//header component
+export function Footer() {
+    return(
+        <footer></footer>
+    );
+}
