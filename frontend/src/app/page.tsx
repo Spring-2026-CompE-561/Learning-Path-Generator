aedc8427ec9acbe384f39bab4bfdb3573242d34b
tuong <ttruong2621@sdsu.edu>
2026-05-04 17:13:51 -0700

    Adding meteor animation window

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
-import Image from "next/image";
+import { MeteorsDemo } from "@/components/meteor";
 
 export default function Home() {
   return (
 
-    <div className="flex w-fill flex-col items-center justify-center gap-8 py-20"> Hello World! </div>
+    <div className="flex w-fill flex-col items-center justify-center gap-8 py-20"> 
+      <MeteorsDemo />
+    </div>
     
   );
 }
